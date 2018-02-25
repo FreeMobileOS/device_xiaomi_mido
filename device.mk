597ad92dfa5232854e3668aa78e8d3c5c3a40bd3
TheScarastic <warabhishek@gmail.com>
2018-02-26 00:11:50 +0530

    mido: Add mido specific init services
    Signed-off-by: Sumit Pundir <pundirsumit11@gmail.com>

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service-custom
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.mido.rc
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/vendor/etc/sensors/sensor_def_qcomdev.conf
