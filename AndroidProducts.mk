82b13c4647762a402d6ceea6882267fcfcd7077d
Satish Patel <tosatishpatel@gmail.com>
2019-03-19 15:30:20 +0530

    change brand to fmo device
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_mido.mk
+    $(LOCAL_DIR)/fmo_mido.mk
--- fmo_mido.mk
+++ fmo_mido.mk
@@ -31,7 +31,7 @@ $(call inherit-product, device/xiaomi/mido/device.mk)
 # Device identifier. This must come after all inclusions
 TARGET_VENDOR := Xiaomi
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := full_mido
+PRODUCT_NAME := fmo_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
